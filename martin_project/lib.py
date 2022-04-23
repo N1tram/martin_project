1e5304a47e26759f4a289c8a4254fed2554fe499
Martin Ebel <martin_ebel@icloud.com>
2022-04-23 15:23:39 +0200

    Changed function to account for wrong input

--- martin_project/lib.py
+++ martin_project/lib.py
@@ -1,12 +1,16 @@
-def try_me(name):
+def try_me(name='Martin'):
     """Hej! Welcome to my function.
     The try me function takes your name as an input
     and returns your name the other way around.
     Example: try_me('Martin') returns 'Nitram'."""
     new_name_list = []
-    for i in range(len(name)):
-        new_name_list.append(name[-i-1].lower())
-    return ''.join(new_name_list).capitalize()
+    if name != 'Martin':
+        for i in range(len(name)):
+            new_name_list.append(name[-i-1].lower())
+            output = ''.join(new_name_list).capitalize()
+    else:
+        output = 'Please provide your name (string) as an input to the function.'
+    return output
 
 if __name__ == "__main__":
-    print(try_me('Martin'))
+    print(try_me('Adam'))
